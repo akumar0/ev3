e08a7de74168c07a9fbedfce6ff4c52ed4078346
Anil Kumar <akumar0@gmail.com>
2020-11-22 23:30:53 -0500

    updates

---
+++ pi-test/testmain.py
@@ -0,0 +1,9 @@
+import os
+import time
+from utility import *
+
+if __name__ == '__main__':     # Program start from here
+    print("testmain()")
+    testTime()
+
+    
---
+++ pi-test/utility.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+testvalue=5
+
+def testTime():
+    print(time.time(), "value=", testvalue)
+
+if __name__ == '__main__':     # Program start from here
+    print("utility")
+    testTime()
