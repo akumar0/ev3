849e306629fb85385d53f404c099de6fc79dcfd0
Anil Kumar <akumar0@gmail.com>
2020-11-23 17:50:38 -0500

    password check

--- web/cgi-bin/login-submit.py
+++ web/cgi-bin/login-submit.py
@@ -7,10 +7,20 @@ cgitb.enable()
 print("Content-Type: text/html")    # HTML is following
 print()                             # blank line, end of headers
 
-print("<head><TITLE>Login Submitted</TITLE></head>")
+print("<head><TITLE>Fitness Login Submitted</TITLE></head>")
 print ("<body>")
-print("<H1>Welcome to the login page</H1>")
+print("<H1>Welcome to the Fitness page</H1>")
 form = cgi.FieldStorage()
+username = form["username"].value
+password = form["password"].value
 print("Username:", form["username"].value)
 print("Password:", form["password"].value)
+
+passwordRev = password[::-1]
+
+if (username == passwordRev):
+    print("<p>Login Succeeded")
+else:
+    print("<p>Invalid username/password")
+
 print("</body>")
